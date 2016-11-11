df024d082870d9c13f827c398a2cc46ffb647764
Brendan Castro <brendan064@hotmail.com>
2016-11-11 15:20:53 -0800

    Cleaned up progress status

--- imageProj.py
+++ imageProj.py
@@ -20,70 +20,74 @@ def main():
         picturePaths.reverse()
     else:
         print 'Proceeding with chronological order.'
-
-    cropPictures(finalImageName, picturePaths)
-
-    print 'Congratulations! Your time sliced picture is now ready!'
-        
     
+    cropPictures(finalImageName, picturePaths)
+    print '100% done.'
+    print 'Congratulations! Your time sliced picture is now ready!'
+
+
 def filenameParse():
     filePaths = []
-
+    
     #getting all contents of current folder
     for root, directories, files in os.walk(os.getcwd()):
         for filename in files:
             filepath = os.path.join(root, filename)
             filePaths.append(filepath)
 
-    pictureNames = []
-    #looking for only .jpg images 
+pictureNames = []
+    #looking for only .jpg images
     for picFileNames in filePaths:
         if picFileNames.endswith(".jpg"):
             pictureNames.append(picFileNames)
-            
-    return pictureNames
+
+return pictureNames
 
 def cropPictures(finalImageName, picturePaths):
-
+    
     #get initial picture dimensions to create new dimensions
     image = Image.open(picturePaths[0])
     finalWidth, finalHeight = image.size
     newImage = Image.new("RGB", (finalWidth, finalHeight))
-
+    
     #based on how many pictures, determine how large of a rectangle to crop
     cropWidth = finalWidth / len(picturePaths)
-
+    
     print 'Starting to construct your new picture. This may take a few minutes.'
     cropLocationX = 0
     #starting at left side, place images across the new blank canvas
     
     for imageIndex in range(0,len(picturePaths)):
         imageToCrop = Image.open(picturePaths[imageIndex])
-
+        
         #crop rectangle that is the 'cropWidth' size
         box = (cropLocationX, 0,(cropLocationX+cropWidth),finalHeight)
         croppedImage = imageToCrop.crop(box)
-
+        
         #paste onto new canvas
         newImage.paste(croppedImage,(cropLocationX,0))
-
+        
         #advance cropLocationX to start where last one finished
         cropLocationX += cropWidth
-
+        
         #let user know it is still working
-        if imageIndex == len(picturePaths)/2:
-            print 'Halfway finished constructing your new picture!'
+        if imageIndex == len(picturePaths)/4:
+            print '25% done.'
+        elif imageIndex == len(picturePaths)/2:
+            print '50% done.'
+        elif imageIndex == (len(picturePaths)/2 + len(picturePaths)/4):
+            print '75% done.'
 
-    newImageName = finalImageName+'.jpg'
+newImageName = finalImageName+'.jpg'
     newImage.save(newImageName)
-
+    
     #move newly created image to new directory to not interfere with time lapse photo names
     moveCreatedImageToDirectory(newImage, newImageName)
-    
+
 def moveCreatedImageToDirectory(newImage, newImageName):
     
     newDirectoryPathName = os.getcwd()+'/NewImageFolder'
-
+    
     if not os.path.exists(newDirectoryPathName):
         #new directory not found, create new
         print 'Creating new folder titled newImageFolder, and placing image inside of it.'
@@ -94,7 +98,7 @@ def moveCreatedImageToDirectory(newImage, newImageName):
         
         #moving new image to new directory
         shutil.move(newImagePath, newDirectoryPathName)
-
+    
     else:
         #finding current location of newly created image
         newImagePath = os.getcwd()+'/'+newImageName
